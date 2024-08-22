7b9109796376cc61729bc31b7584798f296a5232
Tristan A. <tristanagraa@gmail.com>
2024-08-22 12:51:58 +0700

    feat: Enable CORS with allowed origins and methods

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const allowedOriginRegex = /https:\/\/glucoscan\.vercel\.app/;
+
+  const corsOptions = {
+    credentials: true,
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        origin === 'http://localhost:3000' ||
+        allowedOriginRegex.test(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: '*',
+  };
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
