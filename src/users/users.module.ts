d56e3e58984d88565622b3ad7dcb1cf16dc6cf05
Tristan A. <tristanagraa@gmail.com>
2024-09-03 14:00:09 +0700

    chore: Add @nestjs/schedule dependency for scheduling tasks

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
+  imports: [ScheduleModule.forRoot()],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
