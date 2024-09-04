2484a478ee3baf18f0fc27706d9f74dab11854e8
Tristan A. <tristanagraa@gmail.com>
2024-09-04 09:58:40 +0700

    feat: Add ProductModule to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [AuthModule, PrismaModule, ConfigModule.forRoot({
     isGlobal: true,
-  }), UsersModule],
+  }), UsersModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
